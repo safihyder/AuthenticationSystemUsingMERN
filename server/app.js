4621033932300a3a05946443b6423a8466dcdf80
Safi Hyder <86651173+safihyder@users.noreply.github.com>
2024-04-13 17:23:32 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -9,9 +9,14 @@ const port = 5000;
 // res.status(200).json("Served Created");
 // })
 app.use(express.json())
+app.use(cors({
+    origin:['https://image-upload-project-using-mern.vercel.app'],
+    methods:['POST','GET','DELETE'],
+    credentials:true
+},
+            ));
 app.use(cookieParser())
 
-app.use(cors())
 app.use(router)
 app.listen(port,()=>{
     console.log(`The server started on the port no: ${port}`)
